069a899a42d8a2536bd1cb9ea7b04205cc011d22
Boris Berkgaut <berkgaut@gmail.com>
2022-12-05 22:36:38 +0100

    day 4

---
+++ 04/04.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+import Data.Attoparsec.Text (Parser, parseOnly, char, decimal)
+
+pairP :: Parser (Int, Int)
+pairP = do
+    x <- decimal
+    char '-'
+    y <- decimal
+    return (x, y)
+
+lineP :: Parser ((Int, Int), (Int, Int))
+lineP = do
+    first <- pairP
+    char ','
+    second <- pairP
+    return (first, second)
+
+parseLine :: T.Text -> [((Int, Int), (Int, Int))]
+parseLine text = case (parseOnly lineP text) of
+    Left  _error -> []
+    Right result -> [result]
+
+isPairFullyContained (a, b) (x, y) = a >= x && b <= y
+
+isFullyContained (pair1, pair2) =
+    isPairFullyContained pair1 pair2 
+    || isPairFullyContained pair2 pair1
+
+part1 input =
+    length
+    $ filter isFullyContained
+    $ concatMap parseLine
+    $ T.lines
+    $ input
+
+isPairOverlapping (a, b) (x, y) =
+    ((a <= x) && (x <= b)) 
+    || ((a <= y) && (y <= b))
+
+isOverlapping (pair1, pair2) =
+    isPairOverlapping pair1 pair2 || isPairOverlapping pair2 pair1
+
+part2 input =
+    length
+    $ filter isOverlapping
+    $ concatMap parseLine
+    $ T.lines
+    $ input
+
+main :: IO ()
+main = do
+    example <- TIO.readFile "04-example.txt"
+    putStrLn . show . part1 $ example
+    input <- TIO.readFile "04.txt"
+    putStrLn . show . part1 $ input
+    putStrLn . show . part2 $ example
+    putStrLn . show . part2 $ input
